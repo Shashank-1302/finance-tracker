bf286351352bcdf4902301a5234335f9aec3aa40
Shashank Shekhar <shashankshekhar1302@gmail.com>
2023-06-05 22:55:42 +0530

    bug fixes

---
+++ components/SignIn.js
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+
+import { authContext } from "@/lib/store/auth-context";
+
+import { FcGoogle } from "react-icons/fc";
+
+function SignIn() {
+  const { googleLoginHandler } = useContext(authContext);
+
+  return (
+    <main className="container max-w-2xl px-6 mx-auto">
+      <h1 className="mb-6 text-6xl font-bold text-center">Hi :) 👋</h1>
+
+      <div className="flex flex-col overflow-hidden shadow-md shadow-slate-500 bg-slate-200 rounded-2xl">
+       
+        <div className="px-4 py-4">
+          <h3 className="text-2xl text-center">Please sign in to continue</h3>
+
+          <button
+            onClick={googleLoginHandler}
+            className="flex self-start gap-2 p-4 mx-auto mt-6 font-medium text-white align-middle bg-gray-900 rounded-lg"
+          >
+            <FcGoogle className="text-2xl" /> Google
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default SignIn;
